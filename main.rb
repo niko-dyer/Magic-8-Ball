4b8b73792a836bc6ebf8cdd7f0ae3ea1477bb941
Niko Dyer <nikodyer44@gmail.com>
2019-02-14 15:49:58 -0700

    Finished Product

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require "colorize"
 require "pry"
 
 @answers = ["Yes", "No", "Maybe", "Why are you asking me?", "It is Certain", "As I see it, yes", "I wouldn't count on it", "Most Likely", "Ask again later", "My reply is no", "Without a doubt", "Signs point to yes", "Very doubtful"]
+@more_answers = @answers.clone
 
 def split
     puts "🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮"
@@ -13,16 +14,22 @@ def question
     split
     puts " "
     puts "Type a question to recieve an answer, you may leave anytime by typing 'Exit'".colorize(:magenta)
-    @user_question = gets.to_s
-    case @user_question
-    when @user_question == "exit"
-        puts "Thank you stranger!"
-        exit
-    when @user_question == "Exit"
-        puts "Thank you stranger!"
+   
+    user_question = gets.strip.to_s
+    
+    case user_question
+    when "exit"
+        puts "Thank you stranger!".colorize(:red)
         exit 
-    when @user_question == "add answer"
+    when "Exit"
+        puts "Thank you stranger!".colorize(:red)
+        exit 
+    when "add answer"
         add_answer
+    when "answers"
+        display_all
+    when "reset"
+        reset
     else
         answer
     end
@@ -30,16 +37,30 @@ end
 
 def answer
     reply = @answers.sample
-    puts "The Magic 8 Ball says: #{reply}".colorize(:blue)
+    puts "The Magic 8 Ball says: #{reply}".colorize(:cyan)
     sleep(3)
     print `clear`
     question  
 end
 
 def add_answer
-    puts "What would you like to add?"
-    new_answer = gets.to_s
-    @answers << new_answer
+    puts "What would you like to add?".colorize(:cyan)
+    new_answer = gets.to_s.strip
+    @more_answers << new_answer
+    question
+end
+
+def display_all
+    @more_answers.each{ |answer| puts answer.colorize(:green)}
+    sleep(3)
+    print `clear`
+    question
+end
+
+def reset
+    @more_answers = @answers
+    print `clear`
+    question
 end
 
 question
